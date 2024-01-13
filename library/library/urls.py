1305cfd8f7f5b29484390738d52a034c349f07ac
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-01-13 19:50:12 +0200

    distributed order to app

--- library/library/urls.py
+++ library/library/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import homepage_view, login_view, books_view, logout_view, add_book, signin_view, orders_view, \
-                   book_delete, add_author_view, new_author_view, authors_list_view, author_delete, create_order_view, detailed_book, close_order
+from .views import homepage_view, login_view, logout_view, signin_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +26,5 @@ urlpatterns = [
     path('login/', login_view, name='login_page'),
     path('logout/', logout_view, name='logout'),
     path('signin/', signin_view, name='signin_page'),
-    path('order/<int:id>', close_order, name='close_order' )
+    # path('order/<int:id>', close_order, name='close_order' )
 ]
