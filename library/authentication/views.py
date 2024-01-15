55c4fd4ddb6574d234155bbd1453acbdd34bafd4
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-01-15 18:29:52 +0200

    added password confirmation

--- library/authentication/views.py
+++ library/authentication/views.py
@@ -31,21 +31,24 @@ def signin_view(request: HttpRequest):
     if request.method == 'GET':
         return render(request, 'library/signin.html', {'form': SignUpForm})
     elif request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            middle_name = form.cleaned_data.get('middle_name', 'unknown')
+        form = SignUpForm(request.POST) 
+        if not form.is_valid():
+           print(form.errors)
+           print(repr(form.errors))
+           return render(request, 'library/signin.html', {'form': form})
+        
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        middle_name = form.cleaned_data.get('middle_name', 'unknown')
 
-            try:
-                new_user = CustomUser.create(email=email, password=password,
-                                                          first_name=first_name, last_name=last_name,
-                                                          middle_name=middle_name)
-                login(request, new_user)
-                return redirect('homepage')
-            except Exception as e:
-                return HttpResponse(f'<h1>Error\n{e}</h1>')
-        else:
-           return render(request, 'library/signin.html', {'form': SignUpForm, 'error_message': 'Invalid credentials'})
+        try:
+            new_user = CustomUser.create(email=email, password=password,
+                                                        first_name=first_name, last_name=last_name,
+                                                        middle_name=middle_name)
+            login(request, new_user)
+            return redirect('homepage')
+        except Exception as e:
+            return HttpResponse(f'<h1>Error\n{e}</h1>')
+        
